68758c97a0fad61d942c05c1afa4e163b74650df
lin <lin@lin.me>
2017-08-02 10:02:54 +0800

    fix async/regeneratorRuntime error

--- Link.js
+++ Link.js
@@ -4,6 +4,10 @@ Object.defineProperty(exports, "__esModule", {
 	value: true
 });
 
+var _regenerator = require('babel-runtime/regenerator');
+
+var _regenerator2 = _interopRequireDefault(_regenerator);
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _react = require('react');
@@ -46,8 +50,8 @@ var Link = function (_React$Component) {
 		return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = Link.__proto__ || Object.getPrototypeOf(Link)).call.apply(_ref, [this].concat(args))), _this), _this.click = function () {
 			_this.link.click();
 		}, _this.handleClick = function () {
-			var _ref2 = _asyncToGenerator(regeneratorRuntime.mark(function _callee(ev) {
-				return regeneratorRuntime.wrap(function _callee$(_context) {
+			var _ref2 = _asyncToGenerator(_regenerator2.default.mark(function _callee(ev) {
+				return _regenerator2.default.wrap(function _callee$(_context) {
 					while (1) {
 						switch (_context.prev = _context.next) {
 							case 0:
